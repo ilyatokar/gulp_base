7a2c53c8d64baaf3275b9a3e5413ddd31bc9dc51
ilya <itokar98@mail.ru>
2018-08-18 04:09:04 +0300

    added ejs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
 	csso = require('gulp-csso'),
+	ejs = require("gulp-ejs"),
 	brws = require('browser-sync').create();
 
 gulp.task('browser-sync', function(){
@@ -14,7 +15,7 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('styles', function(){
-	return gulp.src('dist/sass/style.scss')
+	return gulp.src('./dist/sass/style.scss')
 	.pipe(sass())
 	.pipe(autoprefixer())
 	.pipe(csso())
@@ -23,13 +24,21 @@ gulp.task('styles', function(){
 });
 
 gulp.task('html', function () {
-	return gulp.src('/dist/*.html')
+	return gulp.src('./dist/*.html')
 	.pipe(brws.stream());
 })
 
-gulp.task('watch', function(){
-	gulp.watch('./dist/sass/**/*.scss', ['styles']);
-	gulp.watch('.dist/*.html', ['html']);
+gulp.task('ejs', function(){
+	gulp.src('dist/pages/*.ejs')
+    .pipe(ejs({ msg: 'Hello Gulp!'}, {}, { ext: '.html' }))
+    .pipe(gulp.dest("dist"))
+    .pipe(brws.stream());
 });
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('watch', function(){
+	gulp.watch('./dist/sass/**/*.scss', ['styles']);
+	gulp.watch('./dist/*.html', ['html']);
+	gulp.watch('./dist/pages/*.ejs', ['ejs']);
+});
+
+gulp.task('default', ['ejs','browser-sync', 'watch']);
